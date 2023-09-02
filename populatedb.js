12b9835fd566c90beae6407f0afac67e3cf63346
David Medeiros <123705989+davmedeiros@users.noreply.github.com>
2023-09-02 14:31:13 +0100

    Refactor populatedb and start adding brands

--- populatedb.js
+++ populatedb.js
@@ -3,56 +3,55 @@
 console.log('This script populates some test categories to the database.');
 console.log('Usage .: node populatedb "<some_connection>"');
 
-// Get arguments passed on command line
 const userArgs = process.argv.slice(2);
 
 const Category = require('./models/category');
+const Brand = require('./models/brand');
 
 const categories = [];
+const brands = [];
 
 const mongoose = require('mongoose');
-mongoose.set('strictQuery', false); // Prepare for Mongoose 7
+mongoose.set('strictQuery', false);
 
 const mongoDB = userArgs[0];
 
-main().catch((err) => console.log(err));
-
-async function main() {
-  console.log('Debug: About to connect');
-  await mongoose.connect(mongoDB);
-  console.log('Debug: Should be connected?');
-  await createCategory();
-  console.log('Debug: Closing mongoose');
-  mongoose.connection.close();
-}
-
-// We pass the index to the ...Create functions so that, for example,
-// genre[0] will always be the Fantasy genre, regardless of the order
-// in which the elements of promise.all's argument complete.
-async function categoryCreate(index, name, description) {
+const createCategory = async (index, name, description) => {
   const category = new Category({ name: name, description: description });
   await category.save();
   categories[index] = category;
   console.log(`Added category: ${name}`);
-}
+};
 
-async function createCategory() {
+const populateCategory = async () => {
   console.log('Adding categories');
   await Promise.all([
-    categoryCreate(
+    createCategory(
       0,
       'String',
       'String instruments—also termed stringed or chordophones—originate or produce sound from vibrating strings.'
     ),
-    categoryCreate(
+    createCategory(
       1,
       'Percussion',
       `These instruments are sounded by striking or scraping the instruments using a beater or rubbing the instruments with one's hand.`
     ),
-    categoryCreate(
+    createCategory(
       2,
       'Keyboard',
       'The keyboard instruments are characterized by standard keyboard, though their operations vary in range and usage.'
     ),
   ]);
-}
+};
+
+const main = async () => {
+  console.log('Debug: About to connect');
+  await mongoose.connect(mongoDB);
+  console.log('Debug: Should be connected?');
+  await populateCategory();
+  // await createBrand();
+  console.log('Debug: Closing mongoose');
+  mongoose.connection.close();
+};
+
+main().catch((err) => console.log(err));
