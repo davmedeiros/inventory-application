963857dc7f76f5fc27b05a5cc829c7e14141c280
David Medeiros <123705989+davmedeiros@users.noreply.github.com>
2023-09-02 15:56:53 +0100

    Await for query

--- controllers/instrumentController.js
+++ controllers/instrumentController.js
@@ -2,7 +2,7 @@ const Instrument = require('../models/instrument');
 const asyncHandler = require('express-async-handler');
 
 exports.instrument_list = asyncHandler(async (req, res, next) => {
-  const allInstruments = Instrument.find().sort({ name: 1 }).exec();
+  const allInstruments = await Instrument.find().sort({ name: 1 }).exec();
   res.render('instrument_list', {
     title: 'Instrument List',
     instrument_list: allInstruments,
