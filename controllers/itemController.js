a0b21813cc1d922589a7aebe46ea36553e547e9d
David Medeiros <123705989+davmedeiros@users.noreply.github.com>
2023-09-02 18:11:27 +0100

    Add item detail to controller and fix typo on view

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -8,3 +8,18 @@ exports.item_list = asyncHandler(async (req, res, next) => {
     item_list: allItems,
   });
 });
+
+exports.item_detail = asyncHandler(async (req, res, next) => {
+  const item = await Item.findById(req.params.id).populate('instrument').exec();
+
+  if (item === null) {
+    const err = new Error('Book copy not found');
+    err.status = 404;
+    return next(err);
+  }
+
+  res.render('item_detail', {
+    title: 'Item:',
+    item: item,
+  });
+});
