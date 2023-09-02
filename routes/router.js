8aed8dea4a43c2620bb21caf37f7fbb48f12af80
David Medeiros <123705989+davmedeiros@users.noreply.github.com>
2023-09-02 17:28:01 +0100

    Create category detail view

--- routes/router.js
+++ routes/router.js
@@ -12,5 +12,6 @@ router.get('/categories', category_controller.category_list);
 router.get('/brands', brand_controller.brand_list);
 router.get('/instruments', instrument_controller.instrument_list);
 router.get('/items', item_controller.item_list);
+router.get('/category/:id', category_controller.category_detail);
 
 module.exports = router;
