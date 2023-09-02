b09d52672869118d48f273fea58790603efe57cd
David Medeiros <123705989+davmedeiros@users.noreply.github.com>
2023-09-02 16:36:33 +0100

    Create items view

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -2,7 +2,7 @@ const Item = require('../models/item');
 const asyncHandler = require('express-async-handler');
 
 exports.item_list = asyncHandler(async (req, res, next) => {
-  const allItems = await Item.find().sort({ name: 1 }).exec();
+  const allItems = await Item.find().populate('instrument').exec();
   res.render('item_list', {
     title: 'Item List',
     item_list: allItems,
--- routes/router.js
+++ routes/router.js
@@ -5,10 +5,12 @@ const index_controller = require('../controllers/indexController');
 const category_controller = require('../controllers/categoryController');
 const brand_controller = require('../controllers/brandController');
 const instrument_controller = require('../controllers/instrumentController');
+const item_controller = require('../controllers/itemController');
 
 router.get('/', index_controller.index);
 router.get('/categories', category_controller.category_list);
 router.get('/brands', brand_controller.brand_list);
 router.get('/instruments', instrument_controller.instrument_list);
+router.get('/items', item_controller.item_list);
 
 module.exports = router;
