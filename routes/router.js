81cee5f89327feeffb06890f10e24ed5b2dc402b
David Medeiros <123705989+davmedeiros@users.noreply.github.com>
2023-09-02 13:14:23 +0100

    Remove some lines

--- routes/router.js
+++ routes/router.js
@@ -4,9 +4,7 @@ const router = express.Router();
 const index_controller = require('../controllers/indexController');
 const category_controller = require('../controllers/categoryController');
 
-/* GET home page. */
 router.get('/', index_controller.index);
-
 router.get('/categories', category_controller.category_list);
 
 module.exports = router;
