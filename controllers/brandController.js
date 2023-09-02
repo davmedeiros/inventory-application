427c01fa9fd6ff7f27cce0f8c017b53bf951b557
David Medeiros <123705989+davmedeiros@users.noreply.github.com>
2023-09-02 19:24:40 +0100

    Add brand creation controller and route

--- controllers/brandController.js
+++ controllers/brandController.js
@@ -1,6 +1,7 @@
 const Brand = require('../models/brand');
 const asyncHandler = require('express-async-handler');
 const Instrument = require('../models/instrument');
+const { body, validationResult } = require('express-validator');
 
 exports.brand_list = asyncHandler(async (req, res, next) => {
   const allBrands = await Brand.find().sort({ name: 1 }).exec();
@@ -31,3 +32,41 @@ exports.brand_detail = asyncHandler(async (req, res, next) => {
 exports.brand_create_get = (req, res, next) => {
   res.render('brand_form', { title: 'Create Brand' });
 };
+
+exports.brand_create_post = [
+  body('name')
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage('Brand name must be specified.')
+    .isAlphanumeric()
+    .withMessage('Brand name has non-alphanumeric characters.'),
+  body('country')
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage('Country must be specified.')
+    .isAlphanumeric()
+    .withMessage('Country has non-alphanumeric characters.'),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const brand = new Brand({
+      name: req.body.name,
+      country: req.body.country,
+    });
+
+    if (!errors.isEmpty()) {
+      res.render('brand_form', {
+        title: 'Create Brand',
+        brand: brand,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      await brand.save();
+      res.redirect(brand.url);
+    }
+  }),
+];
